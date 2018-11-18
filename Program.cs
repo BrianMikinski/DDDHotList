3f57d7b30912074c97cd526ef960cd43894ab342
Brian Mikinski <bmikinski@basisplanning.com>
2018-11-18 10:57:05 -0600

    adding masion class with private constructors and static methods as factory objects

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace DDDHotList
     {
         static void Main(string[] args)
         {
+
+            /**
+             * Ideally for the "house" set of classes, we would use a type of inheritance structure
+             * to share code writing the house name, color, windows, doors, etc to the console.
+             * In these examples, we've largely ingored using inheritance instead focusing on
+             * property getter and setter strategies as opposed to good inheritance practices.
+             */
+
              // dirty house with open, public setters
             Console.WriteLine("Dirty House:");
             var dump = new DirtyHouse(){
@@ -17,17 +25,25 @@ namespace DDDHotList
             Console.WriteLine($"    Color: {dump.Color}");
 
             // standard house with private setters;
-
             Console.WriteLine($"Standard House:");
             var standardHouse = new StandardHouse(18, 10, "Blue");
             standardHouse.SetWindows(5);
 
-            Console.WriteLine($" Color: {standardHouse.Color}");
+            Console.WriteLine($"    Color: {standardHouse.Color}");
 
+            // Clean house with readonly properties
             Console.WriteLine("Clean House:");
             var cleanHouse = new CleanHouse(17, 9, "Light Blue");
 
             Console.WriteLine($"    Color: {cleanHouse.Color}");
+
+            // Mansion with pool
+           
+            var mansionWithPool = Mansion.withPool(27, 19, "Tope");
+            mansionWithPool.ShowHouse();
+
+            var mansionWithoutPool = Mansion.withoutPool(33, 23, "Purple");
+            mansionWithoutPool.ShowHouse();
         }
 
         /// <summary>
@@ -104,14 +120,91 @@ namespace DDDHotList
             public readonly string Color;
         }
 
-        public class Vehicle {
+        /// <summary>
+        /// A mansion object.
+        /// 
+        /// Note: "Factory" style object creation. We are using static methods to standardize the creation
+        /// of certain types of objects. This is combined with private constructors as a powerful technique
+        /// to insure your objects are created correctly and safely
+        /// </summary>
+        public class Mansion 
+        {
+            /// <summary>
+            /// Create a new mansion with a pool
+            /// </summary>
+            /// <param name="windows"></param>
+            /// <param name="doors"></param>
+            /// <param name="color"></param>
+            /// <returns></returns>
+            public static Mansion withPool(int windows, int doors, string color)
+            {
+                return new Mansion(windows, doors, color, true);
+            }
+
+            /// <summary>
+            /// Create a new mansion without a pool
+            /// </summary>
+            /// <param name="windows"></param>
+            /// <param name="doors"></param>
+            /// <param name="color"></param>
+            /// <returns></returns>
+            public static Mansion withoutPool(int windows, int doors, string color)
+            {
+                return new Mansion(windows, doors, color);
+            }
+
+            private Mansion(int windows, int doors, string color, bool hasPool = false)
+            {
+                Windows = windows;
+                Doors = doors;
+                Color = color;
+                HasPool = hasPool;  
+            }
+
+            /// <summary>
+            /// Convert a mansion to a clean house
+            /// </summary>
+            /// <returns></returns>
+            public CleanHouse CleanHouse()
+            {
+                return new CleanHouse(Windows, Doors, Color);
+            }
+
+            public readonly int Windows;
+
+            public readonly int Doors;
+
+            public readonly string Color;
+
+            public readonly bool HasPool;
+
+            public void ShowHouse()
+            {
+                if(HasPool)
+                {
+                    Console.WriteLine("Mansion with Pool:");
+                }
+                else
+                {
+                    Console.WriteLine("Mansion without Pool:");
+                }
+
+                Console.WriteLine($"    Color: {Color}");
+                Console.WriteLine($"    Windows: {Windows}");
+                Console.WriteLine($"    Doors: {Doors}");
+            }
+        }
+
+        public class Vehicle
+        {
 
         }
 
         /// <summary>
         /// Abstract base class for a motor vehicle
         /// </summary>
-        public abstract class MotorVehicle{
+        public abstract class MotorVehicle
+        {
         
         }
     }
